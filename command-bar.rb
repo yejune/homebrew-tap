485cacba9a63720372077f9f70ff97e851ec3707
권승민 <max@superconnect.co.kr>
2025-12-23 17:09:49 +0900

    Update command-bar to v0.0.12

--- command-bar.rb
+++ command-bar.rb
@@ -1,8 +1,8 @@
 class CommandBar < Formula
   desc "Command launcher and schedule reminder app for macOS"
   homepage "https://github.com/yejune/command-bar"
-  url "https://github.com/yejune/command-bar/archive/refs/tags/v0.0.11.tar.gz"
-  sha256 "13aca9f29ad26007bf1b2090f67650cebb28c751d7fbe583e7ceb79c8bcc2467"
+  url "https://github.com/yejune/command-bar/archive/refs/tags/v0.0.12.tar.gz"
+  sha256 "5660daba703425587cb31c246fbd7cbf8793084ad3cdeac4ac6a49ef6045236a"
   license "MIT"
   head "https://github.com/yejune/command-bar.git", branch: "main"
 
