27de13f73dc060576f4cb51863000f6e37771d98
Max <max@blue.tools>
2026-01-07 01:17:12 +0900

    Update git-subclone to v0.0.6

--- git-subclone.rb
+++ git-subclone.rb
@@ -1,8 +1,8 @@
 class GitSubclone < Formula
   desc "Manage nested git repositories with independent push capability"
   homepage "https://github.com/yejune/git-subclone"
-  url "https://github.com/yejune/git-subclone/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "c8d19ea956d3d9785de580dbf3d362e975631bdb413b1184bdf2e1c431209abc"
+  url "https://github.com/yejune/git-subclone/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "09e82ff4ba78a8601cb66c123b1106f02bc5be5e6859545e6dc1eeba947446ef"
   license "MIT"
   head "https://github.com/yejune/git-subclone.git", branch: "main"
 
