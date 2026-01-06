bdbe27b9371f75d64062f37909d0c94eb4f84ac6
Max <max@blue.tools>
2026-01-07 01:09:22 +0900

    Update godo to v0.1.8

--- godo.rb
+++ godo.rb
@@ -1,8 +1,8 @@
 class Godo < Formula
   desc "CLI installer for Do - Claude Code project environment"
   homepage "https://github.com/yejune/do"
-  url "https://github.com/yejune/do/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "2747f3b891f1b4cff7c3975670f11644916c2ad3228f2d626e6e36ff44233f59"
+  url "https://github.com/yejune/do/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "eb2b0e2290c2fb6149d14bcedb16b9fb726dd0aa78b49dbb27dee8cbe52ec3cb"
   license "MIT"
   head "https://github.com/yejune/do.git", branch: "main"
 
