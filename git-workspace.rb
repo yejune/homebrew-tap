1b84c0187e55ff5308b607f49d9859d6ad7a271c
권승민 <max@superconnect.co.kr>
2026-01-08 17:11:04 +0900

    Update git-workspace to v0.2.5

--- git-workspace.rb
+++ git-workspace.rb
@@ -1,8 +1,8 @@
 class GitWorkspace < Formula
   desc "Manage nested git repositories with independent push capability"
   homepage "https://github.com/yejune/git-workspace"
-  url "https://github.com/yejune/git-workspace/archive/refs/tags/v0.2.4.tar.gz"
-  sha256 "15929941a4d973bc441d0938b836832d5841b16ea3e073d835d977dcdd2c9c5d"
+  url "https://github.com/yejune/git-workspace/archive/refs/tags/v0.2.5.tar.gz"
+  sha256 "3d78471eccd59b0dae238a8a4f9031b6de8bb706e36c00c274dc089cbf2af668"
   license "MIT"
   head "https://github.com/yejune/git-workspace.git", branch: "main"
 
