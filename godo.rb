0f0f60cf5d8bda29d576a5bf1f2a66526ed32321
권승민 <max@superconnect.co.kr>
2026-01-08 22:29:59 +0900

    Update godo to v0.1.47

--- godo.rb
+++ godo.rb
@@ -1,8 +1,8 @@
 class Godo < Formula
   desc "CLI installer for Do - Claude Code project environment"
   homepage "https://github.com/yejune/do-focus"
-  url "https://github.com/yejune/do-focus/archive/refs/tags/v0.1.46.tar.gz"
-  sha256 "9af248be517a515fd00c0cb7e14f2dfa57ed31b0da5ad64325bf4ab34943e9d2"
+  url "https://github.com/yejune/do-focus/archive/refs/tags/v0.1.47.tar.gz"
+  sha256 "957151f3507b86ed2a8e5d4ccf3c4b7fbc57124d63f4ffc92d795f68985bef1e"
   license "MIT"
   head "https://github.com/yejune/do-focus.git", branch: "main"
 
