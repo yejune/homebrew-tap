fc58f60aa8147d11ddd9e26c1c0b2be71613b9b0
권승민 <max@superconnect.co.kr>
2026-01-14 21:24:28 +0900

    Update git-multirepo to v0.2.22

--- git-multirepo.rb
+++ git-multirepo.rb
@@ -1,8 +1,8 @@
 class GitMultirepo < Formula
   desc "Multi-repository management tool for nested git repositories with independent push capability"
   homepage "https://github.com/yejune/git-multirepo"
-  url "https://github.com/yejune/git-multirepo/archive/refs/tags/v0.2.21.tar.gz"
-  sha256 "6000651d1ef1bc06fbf8d194e4ade527fb67dbe3d2ca78fad6b69757c46275af"
+  url "https://github.com/yejune/git-multirepo/archive/refs/tags/v0.2.22.tar.gz"
+  sha256 "fafd5cd571125112cfe5385ebeff14d631927b115298eb520e09bb5a4eda3097"
   license "MIT"
   head "https://github.com/yejune/git-multirepo.git", branch: "main"
 
