386f1497df5747ec95df34ed271fd89a83f67934
Max <max@blue.tools>
2026-01-16 01:21:15 +0900

    Update godo to v0.1.72

--- godo.rb
+++ godo.rb
@@ -1,8 +1,8 @@
 class Godo < Formula
   desc "CLI installer for Do - Claude Code project environment"
   homepage "https://github.com/yejune/do-focus"
-  url "https://github.com/yejune/do-focus/archive/refs/tags/v0.1.71.tar.gz"
-  sha256 "6bfcfc63a2fa541f48bdd2cdeec39e2a5a507917a29616913b6a4d02d8bd4952"
+  url "https://github.com/yejune/do-focus/archive/refs/tags/v0.1.72.tar.gz"
+  sha256 "2d465954425a74a7ff22d99f73e6a56550611f0134edf22729db6da49db407a6"
   license "MIT"
   head "https://github.com/yejune/do-focus.git", branch: "main"
 
