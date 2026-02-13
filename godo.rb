c69b2439cb07206d52fdae8e9c036d4e19aa1679
권승민 <max@superconnect.co.kr>
2026-02-13 09:59:54 +0900

    Update godo to v0.2.29

--- godo.rb
+++ godo.rb
@@ -1,8 +1,8 @@
 class Godo < Formula
   desc "CLI installer for Do - Claude Code project environment"
   homepage "https://github.com/yejune/do-focus"
-  url "https://github.com/yejune/do-focus/archive/refs/tags/v0.2.28.tar.gz"
-  sha256 "f90dc867c0879ae569cbfe3dc5e68e5283255c6dff978db39cc54bccd3bc47fd"
+  url "https://github.com/yejune/do-focus/archive/refs/tags/v0.2.29.tar.gz"
+  sha256 "3ad322696a498389b9fd42d437963a45b17ac9237db3bc35947b85268264579c"
   license "MIT"
   head "https://github.com/yejune/do-focus.git", branch: "main"
 
